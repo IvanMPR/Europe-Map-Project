13ef1e7b67576d3a09683352ffbcff0624ab496a
Ivan MPR <ivsanmas@gmail.com>
2021-06-22 13:08:34 +0200

    Extracted main function out of event listener

--- script.js
+++ script.js
@@ -25,45 +25,65 @@ map.addEventListener('mouseout', function (e) {
 
 map.addEventListener('click', function (e) {
   if (e.target.id === 'Layer_1') return;
-  const currentCountry = e.target.id.split('_')[0];
-  console.log(currentCountry);
+  const currentCountry =
+    e.target.id.split('_')[0] === 'United'
+      ? 'United Kingdom'
+      : e.target.id.split('_')[0];
+  console.log(e.target.id.split('_'));
   const getDataFromApi = function (country) {
     fetch(`https://restcountries.eu/rest/v2/name/${country}`)
       .then(function (response) {
         console.log(response);
         return response.json();
       })
-      .then(function (data) {
-        console.log(data);
-        console.log(`url(${data[0].flag})`);
-        /* ********************************************** */
-        flag.style.backgroundImage = `url(${data[0].flag})`;
-        flag.style.backgroundPosition = `center`;
-        flag.style.backgroundSize = `cover`;
-        /* ********************************************** */
-        countryName.textContent = data[0].name;
-        countryContinent.textContent = data[0].subregion;
-        /* ********************************************** */
-        countryPopulation.textContent = `${(
-          data[0].population / 1000000
-        ).toFixed(2)}M`;
-        /* ********************************************** */
-        countryLanguage.textContent =
-          data[0].languages.length > 1
-            ? data[0].languages.map(lang => lang.name)
-            : data[0].languages[0].name;
-        /* ********************************************** */
-        countryCurrency.textContent =
-          data[0].name === 'Bosnia and Herzegovina'
-            ? 'BiH Convertible Mark'
-            : data[0].currencies[0].name;
-        /* ********************************************** */
-        countryCapital.textContent = data[0].capital;
-        countryLocation.textContent = `Lat: ${data[0].latlng[0].toFixed(
-          2
-        )}, Lng: ${data[0].latlng[1].toFixed(2)}`;
-      });
+      .then(data => renderData(data));
   };
 
   getDataFromApi(currentCountry);
 });
+// console.log(
+// window.screen.availHeight - (window.outerHeight - window.innerHeight)
+// );
+// console.log(window.screen.availWidth - (window.outerWidth - window.innerWidth));
+function renderData(data) {
+  console.log(data);
+  console.log(`url(${data[0].flag})`);
+  /* ********************************************** */
+  flag.style.backgroundImage = `url(${data[0].flag})`;
+  flag.style.backgroundPosition = `center`;
+  flag.style.backgroundSize = `cover`;
+  /* ********************************************** */
+  countryName.textContent = data[0].name;
+  countryContinent.textContent = data[0].subregion;
+  /* ********************************************** */
+  countryPopulation.textContent = `${(data[0].population / 1000000).toFixed(
+    2
+  )}M`;
+  /* ********************************************** */
+  if (data[0].name === 'Norway') {
+    countryLanguage.textContent = data[0].languages[0].name;
+  } else {
+    countryLanguage.textContent =
+      data[0].languages.length > 1
+        ? data[0].languages.map(lang => lang.name)
+        : data[0].languages[0].name;
+  }
+  /* ********************************************** */
+  countryCurrency.textContent =
+    data[0].name === 'Bosnia and Herzegovina'
+      ? 'BiH Convertible Mark'
+      : data[0].currencies[0].name;
+  /* ********************************************** */
+  countryCapital.textContent = data[0].capital;
+  countryLocation.textContent = `Lat: ${data[0].latlng[0].toFixed(
+    2
+  )}, Lng: ${data[0].latlng[1].toFixed(2)}`;
+}
+// console.log(
+//   fetch('https://restcountries.eu/rest/v2/name/macedonia')
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .then(data => console.log(data))
+// );
